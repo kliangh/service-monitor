b1a2b0f425b844acae3ba1562b48434cdd414b8a
kliangh <kunliang.hou@gmail.com>
2018-08-07 22:08:21 +0800

    service-monitor 1. logging service information to log file

--- sample/service_monitor.py
+++ sample/service_monitor.py
@@ -1,6 +1,16 @@
 import logging
 
-from src.config_logging import setup_logging
+from sample.config_logging import setup_logging
+from sample.http_utils import trigger_get_request
+from sample.yaml_utils import parse_yaml_to_objects
 
 setup_logging()
-logging.info('Hi')
+
+service_list_path = '../resources/service_list.yml'
+target_object = "Service"
+
+services = parse_yaml_to_objects(service_list_path, target_object)
+
+for service in services:
+    for uri in service.uris:
+        logging.info(trigger_get_request(service.hostname, service.port, uri))
